a338b6b439a4a4f2d094ce4adcb0b1f2d58e25e8
Justin Patterson <justin@scalawag.org>
2013-06-28 22:32:25 -0500

    Simplify test timber configuration

--- src/test/scala/Timber.scala
+++ src/test/scala/Timber.scala
@@ -1,16 +1,10 @@
-import org.scalawag.timber.api.impl.EntryDispatcher
 import org.scalawag.timber.api.impl.TimberConfiguration
-import org.scalawag.timber.impl.dispatcher.SynchronousEntryDispatcher
 import org.scalawag.timber.dsl._
 import org.scalawag.timber.impl.formatter.DefaultEntryFormatter
 
 object Timber extends TimberConfiguration {
-  lazy val dispatcher = new SynchronousEntryDispatcher {
-    configure { IN =>
-      implicit val formatter = new DefaultEntryFormatter(headerOnEachLine = true)
-      IN :: file("target/test.log")
-    }
-  }
+  override val formatter = new DefaultEntryFormatter(headerOnEachLine = true)
+  override val receiver = file("target/test.log")(formatter)
 }
 
 /* druthers -- Copyright 2013 Justin Patterson -- All Rights Reserved */
