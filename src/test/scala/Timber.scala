5299585247a8d09ee12cb9b06178bb050f75d8aa
Justin Patterson <justin@scalawag.org>
2013-06-30 12:28:35 -0500

    Use AutoFlush for test logging

--- src/test/scala/Timber.scala
+++ src/test/scala/Timber.scala
@@ -1,10 +1,12 @@
+import java.io.{FileWriter, File}
 import org.scalawag.timber.api.impl.TimberConfiguration
 import org.scalawag.timber.dsl._
 import org.scalawag.timber.impl.formatter.DefaultEntryFormatter
+import org.scalawag.timber.impl.receiver.{WriterReceiver, AutoFlush, FileAppender}
 
 object Timber extends TimberConfiguration {
   override val formatter = new DefaultEntryFormatter(headerOnEachLine = true)
-  override val receiver = file("target/test.log")(formatter)
+  override val receiver = new WriterReceiver(new FileWriter("target/test.log"),formatter) with AutoFlush
 }
 
 /* druthers -- Copyright 2013 Justin Patterson -- All Rights Reserved */
