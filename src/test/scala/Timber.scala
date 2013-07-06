f33309e1c77dc735ae217481a93b5fe8bc8f49cb
Justin Patterson <justin@scalawag.org>
2013-07-06 16:58:35 -0500

    Upgrading to latest timber 0.3-SNAPSHOT

--- src/test/scala/Timber.scala
+++ src/test/scala/Timber.scala
@@ -5,8 +5,8 @@ import org.scalawag.timber.impl.formatter.DefaultEntryFormatter
 import org.scalawag.timber.impl.receiver.{WriterReceiver, AutoFlush, FileAppender}
 
 object Timber extends TimberConfiguration {
-  override val formatter = new DefaultEntryFormatter(headerOnEachLine = true)
-  override val receiver = new WriterReceiver(new FileWriter("target/test.log"),formatter) with AutoFlush
+  override lazy val formatter = new DefaultEntryFormatter(headerOnEachLine = true)
+  override lazy val receiver = new WriterReceiver(new FileWriter("target/test.log"),formatter) with AutoFlush
 }
 
 /* druthers -- Copyright 2013 Justin Patterson -- All Rights Reserved */
